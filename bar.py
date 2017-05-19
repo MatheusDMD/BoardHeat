a12811e107bf09c3570e391ec9008069334c8daa
marcelogdeandrade <marcelogdeandrade@gmail.com>
2017-05-19 09:27:40 -0300

    removes fourier function and returns from main if is bigger than 0.25

--- bar.py
+++ bar.py
@@ -22,10 +22,6 @@ def import_file(file_name):
             content[i][1] = int(content[i][1])
     return dict(content)
 
-def calc_fourier_number(alpha, d_x, d_t):
-    """Calc fourier number."""
-    return alpha * d_t / (d_x**2)
-
 
 def calc_item(list_prev, i, j, mult_lambda):
     """Calc temp to item."""
@@ -133,9 +129,10 @@ def main():
     """Main func."""
     global d, mult_lambda, im, list_prev
     d = import_file("info.txt")
-    fourier_number = calc_fourier_number(d["alpha"], d["d_x"], d["d_t"])
+    fourier_number = calc_mult_lambda(d["alpha"], d["d_x"], d["d_t"])
     if (fourier_number > 0.25):
         print("Fourier Number is {0}, we don't ensure that your simulation converges.".format(fourier_number))
+        return
     d["row"] = int( d["row_size"]/d["d_x"])
     d["col"] = int( d["col_size"]/d["d_x"])
     print(d["row"])
